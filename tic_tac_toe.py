0afbd3028e816869475849eb17cc0ac1b7c55255
mApS-bit <psmarco832@gmail.com>
2025-08-18 15:36:08 -0600

    Add Game Menu

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -85,6 +85,14 @@ class TicTacToeGame:
      def toggle_player(self):
           '''Return a toggled player'''
           self.current_player = next(self._players)
+
+     def reset_game(self):
+          '''Reset the game state to play again.'''
+          for row, row_content in enumerate(self._current_moves):
+               for col, _ in enumerate(row_content):
+                    row_content[col] = Move(row, col)
+          self._has_winner = False
+          self.winner_combo = []
      
 
 class TicTacToeBoard(tk.Tk):
@@ -93,6 +101,7 @@ class TicTacToeBoard(tk.Tk):
         self.title("Tit-Tac-Toe Game")
         self._cells = {}
         self._game = game
+        self._create_menu()
         self._create_board_display()
         self._create_board_grid()
 
@@ -164,7 +173,24 @@ class TicTacToeBoard(tk.Tk):
          for button, coordinates in self._cells.items():
               if coordinates in self._game.winner_combo:
                    button.config(highlightbackground="red")
-             
+    
+    def _create_menu(self):
+         menu_bar = tk.Menu(master=self)
+         self.config(menu=menu_bar)
+         file_menu = tk.Menu(master=menu_bar)
+         file_menu.add_command(label='Play Again', command=self.reset_board)
+         file_menu.add_separator()
+         file_menu.add_command(label='Exit', command=quit)
+         menu_bar.add_cascade(label='File', menu=file_menu)
+    
+    def reset_board(self):
+         '''Reset board to play again'''
+         self._game.reset_game()
+         self._update_display(msg="Ready")
+         for button in self._cells.keys():
+              button.config(highlightbackground="lightblue")
+              button.config(text="")
+              button.config(fg="black") 
     
 def main():
         '''Creates the game's board and run its main loop'''
